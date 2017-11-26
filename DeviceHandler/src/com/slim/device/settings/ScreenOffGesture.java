e9432fef00c3d6839d48205d68bb01f0605eb4ab
rc420head <dennislebhardt79@gmail.com>
2017-11-25 23:39:49 -0600

    aquarios

--- DeviceHandler/src/com/slim/device/settings/ScreenOffGesture.java
+++ DeviceHandler/src/com/slim/device/settings/ScreenOffGesture.java
@@ -37,10 +37,10 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
-import com.android.internal.util.gzosp.AppHelper;
-import com.android.internal.util.gzosp.ActionConstants;
-import com.android.internal.util.gzosp.DeviceUtils;
-import com.android.internal.util.gzosp.DeviceUtils.FilteredDeviceFeaturesArray;
+import com.android.internal.util.aquarios.AppHelper;
+import com.android.internal.util.aquarios.ActionConstants;
+import com.android.internal.util.aquarios.DeviceUtils;
+import com.android.internal.util.aquarios.DeviceUtils.FilteredDeviceFeaturesArray;
 
 import com.slim.device.KernelControl;
 import com.slim.device.R;
